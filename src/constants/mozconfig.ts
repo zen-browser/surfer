b48a48509eda3ef3c097d5850645a0cbcb7da5b4
Mauro Balades <mauro.balades@tutanota.com>
2024-07-28 23:36:15 +0200

    chore: Update platform optimization flags for Linux

--- src/constants/mozconfig.ts
+++ src/constants/mozconfig.ts
@@ -67,7 +67,7 @@ function getPlatformOptimiseFlags(): string {
 
   switch ((process as any).surferPlatform) {
     case 'linux': {
-      optimiseFlags = `ac_add_options --enable-optimize="-march=x86-64-v3 -msse3 -mtune=haswell -O3 -w -mavx -maes"`
+      optimiseFlags = `ac_add_options --enable-optimize="-march=nehalem -mtune=znver3 -msse3 -O3 -w -mavx -maes"`
       break
     }
     case 'darwin': {
