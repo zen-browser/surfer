af7accb25cd315f8044657811174ee87c16bedb4
Mauro Balades <mauro.balades@tutanota.com>
2024-08-02 00:51:18 +0200

    chore: Update package.json version to 1.2.5 and fix compatibility mode flag

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ export const bin_name = BIN_NAME
 
 const programVersions = []
 
-export const compatMode = process.env.SURFER_COMPAT == '1';
+export const compatMode = process.env.SURFER_COMPAT == 'true';
 
 for (const brand in config.brands) {
   const brandConfig = config.brands[brand]
