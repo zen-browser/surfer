d0b14d63206175bec8e11748f0c9f8828b5263a8
mauro-balades <mauro.balades@tutanota.com>
2024-09-22 16:48:55 +0200

    Update devtools.selfxss.count preference to enable pasting code into the web console

--- src/commands/patches/branding-patch.ts
+++ src/commands/patches/branding-patch.ts
@@ -383,7 +383,7 @@ pref("app.releaseNotesURL.prompt", "https://zen-browser.app/release-notes/%VERSI
 
 // Number of usages of the web console.
 // If this is less than 5, then pasting code into the web console is disabled
-pref("devtools.selfxss.count", 0);
+pref("devtools.selfxss.count", 5);
 `);
 }
 
